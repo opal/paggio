2b257132a4a9b4e6eb42a2caa9e8519ccc94fd62
meh <meh@schizofreni.co>
2013-12-15 23:35:33 +0100

    spec: add initial tests

---
+++ Rakefile
@@ -0,0 +1,16 @@
+#! /usr/bin/env ruby
+require 'rake'
+
+task :default => [:install, :test]
+
+task :install do
+	sh 'gem install --no-force rspec'
+	sh 'gem build *.gemspec'
+	sh 'gem install *.gem'
+end
+
+task :test do
+	FileUtils.cd 'spec' do
+		sh 'rspec css_spec.rb --backtrace --color --format doc'
+	end
+end
---
+++ spec/css_spec.rb
@@ -0,0 +1,41 @@
+require 'paggio'
+
+describe Paggio::CSS do
+  it 'builds a rule' do
+    css = Paggio.css do
+      rule '#lol' do
+        color 'black'
+      end
+    end
+
+    css.to_s.should == "#lol {\n\tcolor: black;\n}\n"
+  end
+
+  it 'builds border-radius correctly' do
+    css = Paggio.css do
+      rule '#lol' do
+        border radius: '5px'
+      end
+    end
+
+    css.to_s.should == "#lol {\n\t-moz-border-radius: 5px;\n\t-webkit-border-radius: 5px;\n\tborder-radius: 5px;\n}\n"
+
+    css = Paggio.css do
+      rule '#lol' do
+        border radius: { top: { left: '5px' } }
+      end
+    end
+
+    css.to_s.should == "#lol {\n\t-moz-border-radius-topleft: 5px;\n\t-webkit-border-top-left-radius: 5px;\n\tborder-top-left-radius: 5px;\n}\n"
+  end
+
+  it 'builds box-shadow correctly' do
+    css = Paggio.css do
+      rule '#lol' do
+        box shadow: '0 0 5px black'
+      end
+    end
+
+    css.to_s.should == "#lol {\n\t-moz-box-shadow: 0 0 5px black;\n\t-webkit-box-shadow: 0 0 5px black;\n\tbox-shadow: 0 0 5px black;\n}\n"
+  end
+end
